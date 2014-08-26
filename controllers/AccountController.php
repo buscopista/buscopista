6aad13d75bd3b012b3eecaa32b79e2ab585e5266
Pepe Castelltort <pepe.84@gmail.com>
2014-08-26 18:06:02 +0200

    Split account and password settings

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -58,53 +58,54 @@ class AccountController extends Controller
             return $this->redirect(['/account/login']);
         }
         
-        // Individual forms!!!
-        $modelA = new AccountForm();
-        $modelP = new PasswordForm();
+        $model = new AccountForm();
         
         // Default values
-        $tab = 'account';
-        $user = clone Yii::$app->user->getIdentity();
-        $modelA->setAttributes(array(
-            'username' => $user->username,
-            'mail'     => $user->mail,
+        $userCopy = clone Yii::$app->user->getIdentity();
+        $model->setAttributes(array(
+            'username' => $userCopy->username,
+            'mail'     => $userCopy->mail,
         ));
         
-        switch (TRUE) {
-            // Update account?
-            case $modelA->load(Yii::$app->request->post()):
-                $tab = 'account';
-                if ($modelA->update()) { 
-                    Yii::$app->session->setFlash('success', Yii::t('app', 'Your account has been successfully updated.'));
-                    // If user changes mail...
-                    if ($user->mail !== $modelA->mail) {
-                        // Deactive user
-                        $user->deactivate();
-                        // Send confirmation mail again
-                        $model = new RegisterForm();
-                        $model->sendConfirmationMail($user);
-                        // Logout
-                        Yii::$app->user->logout();
-                        // TODO show a message...
-                        return $this->goHome();
-                    }
-                }
-                break;
-            // Update password?
-            case $modelP->load(Yii::$app->request->post()):
-                $tab = 'password';
-                if ($modelP->update()) {
-                    Yii::$app->session->setFlash('success', Yii::t('app', 'Your password has been successfully changed.'));
-                } else {
-                    $modelP->reset(); // Security issues...
-                }
-                break;
+        if ($model->load(Yii::$app->request->post()) && $model->update()) {
+            Yii::$app->session->setFlash('success', Yii::t('app', 'Your account has been successfully updated.'));
+            // If user changes mail...
+            if ($userCopy->mail !== $model->mail) {
+                // Deactive user
+                $userCopy->deactivate();
+                $userCopy->save();
+                // Send confirmation mail again
+                $obj = new RegisterForm();
+                $obj->sendConfirmationMail($userCopy);
+                // Logout
+                Yii::$app->user->logout();
+                // TODO show a message...
+                return $this->goHome();
+            }            
         }
         
         return $this->render('index', [
-            'modelA' => $modelA,
-            'modelP' => $modelP,
-            'tab'    => $tab
+            'model' => $model
+        ]);
+    }
+    
+    public function actionPassword()
+    {
+        if (\Yii::$app->user->isGuest) {
+            Yii::$app->session->setFlash('error', Yii::t('app', 'You need to login first'));
+            return $this->redirect(['/account/login']);
+        }
+        
+        $model = new PasswordForm();
+        
+        if ($model->load(Yii::$app->request->post()) && $model->update()) {
+            Yii::$app->session->setFlash('success', Yii::t('app', 'Your password has been successfully changed.'));
+        } else {
+            $model->reset(); // Security issues...
+        }
+        
+        return $this->render('password', [
+            'model' => $model,
         ]);
     }
     
@@ -134,16 +135,13 @@ class AccountController extends Controller
             $user ? $user->setScenario(User::SCENARIO_CONFIRM) : null;
             $token = Yii::$app->request->get('token');
             
-            // Activate user account
             if ($user && $token && $user->confirmToken === $token) { 
-                if ($user->activate() && $user->save()) {
-                    Yii::$app->session->setFlash('success', Yii::t('app', 'Account succesfully verified. Please enter your credentials to sign in.'));
-                    return $this->redirect(['/account/login']);
-                } else {
-                    Yii::error("Unable to activate user '{$username}' using confirmToken '{$token}'");
-                    Yii::$app->session->setFlash('error', Yii::t('app', 'Unexpected error. Please contact to support team for more information.'));
-                    return $this->goHome();
-                }
+                // Activate user
+                $user->activate();
+                $user->save();
+                // Notify user
+                Yii::$app->session->setFlash('success', Yii::t('app', 'Account succesfully verified. Please enter your credentials to sign in.'));
+                return $this->redirect(['/account/login']);
             } else {
                 Yii::error("Username '{$username}' and confirmToken '{$token}' doesn't match");
                 Yii::$app->session->setFlash('error', Yii::t('app', 'Wrong URL params. Please contact to support team for more information.'));
--- views/account/password.php
+++ views/account/password.php
@@ -6,26 +6,27 @@ use yii\widgets\ActiveForm;
 /**
  * @var yii\web\View $this
  * @var yii\widgets\ActiveForm $form
- * @var app\models\AccountForm $model
+ * @var app\models\PasswordForm $model
  */
 $this->title = Yii::t('app', 'Settings');
 $this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = Yii::t('app', 'Change password');
 ?>
-<div class="account-index">
+<div class="account-password">
     
     <h1><?= Html::encode($this->title) ?></h1>
     
     <!-- Nav tabs -->
     <ul id="settings-tabs" class="nav nav-tabs" role="tablist">
-      <li class="active"><a href="#account" role="tab" data-toggle="tab"><?= Html::encode(Yii::t('app', 'Account')) ?></a></li>
-      <li><a href="<?= Url::to(['/account/password']) ?>" role="tab"><?= Html::encode(Yii::t('app', 'Password')) ?></a></li>
+      <li><a href="<?= Url::to(['/account']) ?>" role="tab"><?= Html::encode(Yii::t('app', 'Account')) ?></a></li>
+      <li class="active"><a href="#password" role="tab" data-toggle="tab"><?= Html::encode(Yii::t('app', 'Password')) ?></a></li>
     </ul>
 
     <!-- Tab panes -->
     <div class="tab-content">
-        <p><?= Html::encode(Yii::t('app', 'Change your basic account and language settings.')) ?></p>
+        <p><?= Html::encode(Yii::t('app', 'Change your password or recover your current one.')) ?></p>
         <?php $form = ActiveForm::begin([
-            'id' => 'account-form',
+            'id' => 'password-form',
             'options' => ['class' => 'form-horizontal'],
             'fieldConfig' => [
                 'template' => "{label}\n<div class=\"col-lg-3\">{input}\n{hint}</div>\n<div class=\"col-lg-8\">{error}</div>",
@@ -33,13 +34,14 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ]); ?>
 
-        <?= $form->field($model, 'username') ?>
-        <?= $form->field($model, 'mail')
-                 ->hint(Yii::t('app', 'If you change your e-mail address, you will need to confirm your account again after automatic logout.')) ?>
+        <?= $form->field($model, 'old_password')->passwordInput()
+                 ->hint(Html::a(Yii::t('app', 'Forgot password?'), ['/account/forgot'])) ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'repeat_password')->passwordInput() ?>
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton(Yii::t('app', 'Save changes'), [
+                <?= Html::submitButton(Yii::t('app', 'Change password'), [
                     'class' => 'btn btn-primary col-lg-3', 
                     'name' => 'login-button'
                 ]) ?>
